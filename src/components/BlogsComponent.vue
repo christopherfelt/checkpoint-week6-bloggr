98a230843b06a013f6348bb8ebee04fb7819f209
chris_MSI1 <chris.h.felt@gmail.com>
2020-06-05 13:04:51 -0600

    init

---
+++ src/components/BlogsComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="blogs-component">
+    <h1>Blog Component is Live</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogComponent",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped></style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Welcome</h1>
+  </div>
+</template>
+
+<script>
+import BlogComponent from "@/components/BlogsComponent.vue";
+
+export default {
+  name: "home",
+  components: {
+    BlogComponent,
+  },
+};
+</script>
